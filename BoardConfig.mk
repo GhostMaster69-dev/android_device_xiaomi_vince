c018d4572ff912dc1ce9803e948743e3b421cfda
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    vince: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: I8124a5f92a75490ce48422b9d760764733efe37e
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -181,4 +181,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/xiaomi/vince/BoardConfigVendor.mk
+include vendor/xiaomi/vince/BoardConfigVendor.mk

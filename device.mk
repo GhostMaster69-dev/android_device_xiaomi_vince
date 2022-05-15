ad6a0f8488f88d6444208910fbc945340140c286
ZeeLog <Danjel.Zammit@gmail.com>
2022-05-15 12:58:45 +0200

    vince: Update Display stack from LA.UM.10.6.2.r1-01600-89xx.0
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -206,7 +206,9 @@ PRODUCT_PACKAGES += \
     libqdMetaData \
     libvulkan \
     vendor.display.config@1.0.vendor \
-    vendor.display.config@2.0
+    vendor.display.config@2.0 \
+    vendor.qti.hardware.display.allocator-service \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # Dex preopt
 ifeq ($(WITH_GAPPS),true)

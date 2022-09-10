f4a46b066d343facb930a8580cb7ef7b9f4ef3cb
nicknitewolf <nicknitewolf@lineageos.org>
2022-09-10 10:57:12 +0530

    vince: Drop QTI ICryptfsHw 1.0
    Signed-off-by: anandhan07 <anandhasayanan007jb@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -280,10 +280,6 @@ PRODUCT_PACKAGES += \
     android.hidl.manager@1.0 \
     android.hidl.base@1.0.vendor
 
-# HW crypto
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
-
 # IMS
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \

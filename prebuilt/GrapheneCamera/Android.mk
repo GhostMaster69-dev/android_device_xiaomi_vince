bf4705921f8d2d12f4246086fc7ddaed7cf75e45
GhostMaster69-dev <rathore6375@gmail.com>
2022-10-04 23:15:59 +0530

    vince: Move GrapheneCamera outside of hw-qcom-cam dir
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- prebuilt/GrapheneCamera/Android.mk
+++ prebuilt/GrapheneCamera/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := GrapheneCamera
 LOCAL_MODULE_CLASS := APPS
 LOCAL_PRODUCT_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := prebuilt/Camera.apk
+LOCAL_SRC_FILES := Camera.apk
 LOCAL_OPTIONAL_USES_LIBRARIES := androidx.camera.extensions.impl
 LOCAL_OVERRIDES_PACKAGES := Camera Camera2
 

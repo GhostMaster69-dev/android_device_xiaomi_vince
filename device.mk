61234411b96ed6f5dbd728edcb7af1a7ad08a9c5
Michael Bestas <mkbestas@lineageos.org>
2022-11-04 05:26:31 +0100

    vince: Move qti_whitelist.xml to /system_ext
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -473,7 +473,7 @@ PRODUCT_COPY_FILES += \
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/camera-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/camera-hiddenapi-package-whitelist.xml
 
 # Wifi

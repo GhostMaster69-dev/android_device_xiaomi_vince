4dbe363e704ead71b7e3e7e11abb27446408e822
Jake Weinstein <jake@aospa.co>
2022-11-17 16:10:32 +0900

    vince: configs: Add default notification permission for Google Clock
    Otherwise, alarms do not trigger.
    
    Change-Id: Id69905d64365fe985e878f26f871a6404155af69
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -261,6 +261,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml
 
+# Google Deskclock permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/default-permissions/default-permissions_com.google.android.deskclock.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions_com.google.android.deskclock.xml
+
 # Google GMS Game Service
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sysconfig/game_service.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/game_service.xml

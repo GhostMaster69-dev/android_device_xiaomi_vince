e6bf86020e91ae811425908f41cecef813b0967a
GhostMaster69-dev <rathore6375@gmail.com>
2022-12-07 20:46:26 +0530

    vince: Ship prebuilt Google CarrierLocation for Android 13
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -240,6 +240,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libwifi-hal-ctrl
 
+# Google CarrierLocation
+PRODUCT_PACKAGES += \
+    GoogleCarrierLocation
+
 # Google Dialer Call recording
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/com.google.android.apps.dialer.call_recording_audio.features.xml

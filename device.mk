7ba1a44e3992d971d9db963e61844cc213a0aa57
me-cafebabe <me.cafebabe@gmail.com>
2023-03-02 23:09:28 +0800

    vince: Restart qcrild on modem subsystem restart
    * For some SIM carriers, when moving between areas, the modem subsystem crashes and restarts
    * Mobile Data doesn't work without restarting qcrild after modem subsystem restarts
    
    Change-Id: I8545ba88b94aaccc8c851998752c46431ce71417
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- device.mk
+++ device.mk
@@ -444,6 +444,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
+# Subsystem state notifier
+PRODUCT_PACKAGES += \
+    subsystem_state_notifier
+
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf

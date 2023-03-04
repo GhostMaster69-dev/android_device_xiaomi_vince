17bd06e50cb5d1458122da85f23de386f3f42989
GhostMaster69-dev <rathore6375@gmail.com>
2023-03-04 09:58:41 +0530

    vince: hals: Disable various clang warnings for unused variables.
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS :=  -Wno-compound-token-split-by-macro \
+LOCAL_CFLAGS :=  -Wno-unused-variable -Wno-compound-token-split-by-macro \
                    -g -O0
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../qexif

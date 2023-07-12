f8c8fb3f0473bb009dc9787d684abea9f9b188d7
GhostMaster69-dev <rathore6375@gmail.com>
2023-07-12 20:43:48 +0530

    vince: RemovePackages: Update packages and Remove Gmail2
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- packages/apps/RemovePackages/Android.mk
+++ packages/apps/RemovePackages/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := /dev/null
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_OVERRIDES_PACKAGES := AndroidAutoStub CalculatorGoogle CalendarGoogle Chrome DeskClockGoogle Gallery2 GoogleFeedback GooglePrintRecommendationService GoogleTTS talkback Velvet
+LOCAL_OVERRIDES_PACKAGES := AndroidAutoStub CalculatorGoogle CalendarGoogle Chrome64 DeskClockGoogle Gallery2 Gmail2 GoogleFeedback GooglePrintRecommendationService SpeechServicesByGoogle talkback Velvet
 LOCAL_UNINSTALLABLE_MODULE := true
 
 include $(BUILD_PREBUILT)

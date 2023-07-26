a17334048c8e224695db2de344b5016f9047ef59
GhostMaster69-dev <rathore6375@gmail.com>
2023-07-26 21:26:22 +0530

    vince: Provide needed Prima Wlan firmware files from vendor
    - Provide these files directly instead of symbolic links
    - Move WCNSS_cfg.dat to vendor
    
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- Android.mk
+++ Android.mk
@@ -27,28 +27,7 @@ $(WCNSS_INI_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	      @rm -rf $@
 	      $(hide) ln -sf /vendor/etc/wifi/$(notdir $@) $@
 
-WCNSS_BIN_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
-$(WCNSS_BIN_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	      @echo "WCNSS bin link: $@"
-	      @mkdir -p $(dir $@)
-	      @rm -rf $@
-	      $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
-WCNSS_DAT_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/WCNSS_wlan_dictionary.dat
-$(WCNSS_DAT_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	      @echo "WCNSS dat link: $@"
-	      @mkdir -p $(dir $@)
-	      @rm -rf $@
-	      $(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
-WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/wlan_mac.bin
-$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-		@echo "WCNSS MAC bin link: $@"
-		@mkdir -p $(dir $@)
-		@rm -rf $@
-		$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_BIN_SYMLINK) $(WCNSS_DAT_SYMLINK) $(WCNSS_MAC_SYMLINK)
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK)
 
 # A/B builds require us to create the mount points at compile time.
 # Just creating it for all cases since it does not hurt.
